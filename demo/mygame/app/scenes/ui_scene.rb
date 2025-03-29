8c4dd82654b870a9583481a64691dd4361ee902d
Daniel Dye <dnitemaeric@gmail.com>
2025-03-29 21:42:45 +0000

    Add various updates to UIScene

--- demo/mygame/app/scenes/ui_scene.rb
+++ demo/mygame/app/scenes/ui_scene.rb
@@ -1,74 +1,80 @@
 class UIScene < Conjuration::Scene
   TILE_SIZE = 40
 
-  attr_reader :party, :skills, :minimap
+  attr_reader :background, :tooltip, :party, :skills, :minimap
 
   def setup
     gtk.set_cursor "sprites/cursor-none.png", 9, 4
 
     add_camera(:main, x: 0, y: 0)
 
-    state.cells = []
+    @background = Conjuration::UI.build(grid.rect, direction: :row) do
+      (grid.w / TILE_SIZE).to_i.times do |column|
+        node({ w: TILE_SIZE }) do
+          (grid.h / TILE_SIZE).to_i.times do |row|
+            node({
+              w: TILE_SIZE,
+              h: TILE_SIZE,
+              r: (column + row) % 2 == 0 ? 93 : 83,
+              g: (column + row) % 2 == 0 ? 95 : 85,
+              b: (column + row) % 2 == 0 ? 99 : 89,
+              path: :pixel
+            })
+          end
+        end
+      end
+    end
 
-    (grid.w / TILE_SIZE).to_i.times do |column|
-      (grid.h / TILE_SIZE).to_i.times do |row|
-        state.cells << {
-          x: column * TILE_SIZE,
-          y: row * TILE_SIZE,
-          w: TILE_SIZE,
-          h: TILE_SIZE,
-          r: (column + row) % 2 == 0 ? 93 : 83,
-          g: (column + row) % 2 == 0 ? 95 : 85,
-          b: (column + row) % 2 == 0 ? 99 : 89,
-          path: :pixel
-        }
+    @back_button = Conjuration::UI.build({ x: 20, y: 20.from_top, anchor_y: 1 }) do
+      node({ w: 100, h: 50, path: "sprites/button.png", action: -> { change_scene(to: MenuScene.new(:main)) }}, justify: :center, align: :center) do
+        node({ text: "Back", r: 255, g: 255, b: 255 })
+      end
+    end
+
+    @tooltip = Conjuration::UI.build({ x: grid.w, y: grid.h, path: :pixel, w: 200, h: 100, r: 0, g: 0, b: 0 }, padding: 20) do
+      node({ text: "Tooltip", r: 255, g: 255, b: 255 })
+    end
+
+    @party = Conjuration::UI.build({ x: 0, y: grid.h / 2, w: 240, h: grid.h - 200, anchor_y: 0.5, path: "sprites/sidebar-container-background.png", r: 222, g: 222, b: 222 }, align: :stretch) do
+      node({ primitive_marker: :border, h: grid.h - 200 }, justify: :end, align: :stretch, padding: 20, gap: 20) do
+        node({ primitive_marker: :border, h: 200 }, id: :section_1, padding: 20) do
+          node({ text: "Hello, World!" }, id: :sub_section_1)
+        end
+
+        node({ h: 20 }, id: :section_2, align: :end) do
+          node({ text: "Hello, World!" })
+        end
+
+        node({ path: "sprites/button.png", h: 50, action: -> { puts "Button clicked!" } }, id: :button, align: :center, padding: 15) do
+          node(
+            {
+              text: "Click me!",
+              r: 255,
+              g: 255,
+              b: 255
+            },
+            id: :label
+          )
+        end
       end
     end
   end
 
-  def update
-    @party = Conjuration::UI.build({ id: "root", **grid.rect, w: 240 + Math.sin(Kernel.tick_count.to_radians * 2)* 50, y: grid.h / 2, h: grid.h - 200, anchor_y: 0.5, primitive_marker: :border }, padding: 20, gap: 20) do
-      node({ id: "section_1", primitive_marker: :border, h: 200 }, padding: 20) do
-        node({
-          id: "sub_section_1",
-          text: "Hello, World!",
-          primitive_marker: :label,
-          r: 255,
-          g: 255,
-          b: 255,
-          w: 100,
-          h: 20
-        })
-      end
+  def input
+    focused_button = @back_button.find_interactive_intersect(inputs.mouse) || @party.find_interactive_intersect(inputs.mouse)
 
-      node({
-        id: "section_2",
-        text: "Hello, World!",
-        primitive_marker: :label,
-        r: 255,
-        g: 255,
-        b: 255,
-        w: 100,
-        h: 20
-      })
+    if focused_button
+      gtk.set_cursor "sprites/hand-point.png", 6, 4
 
-      node({
-        id: "button",
-        path: "sprites/button.png",
-        primitive_marker: :sprite,
-        h: 50
-      }, alignment: :center, padding: 15) do
-        node({
-          id: "label",
-          text: "Click me!",
-          primitive_marker: :label,
-          r: 255,
-          g: 255,
-          b: 255,
-          w: gtk.calcstringbox("Click me!")[0]
-        })
-      end
+      instance_exec(&focused_button.action) if inputs.mouse.click
+    else
+      gtk.set_cursor "sprites/cursor-none.png", 9, 4
     end
+  end
+
+  def update
+    @party.object.w = 240 + Math.sin(Kernel.tick_count.to_radians * 2) * 40
+    @party.calculate_layout
 
     @skills = Conjuration::UI::Node.new(
       {
@@ -123,18 +129,32 @@ class UIScene < Conjuration::Scene
       direction: :column,
       gap: 20
     )
+
+    tooltip.x, tooltip.y = inputs.mouse.x + 20, inputs.mouse.y + 20
+    if inputs.mouse.x > grid.w - tooltip.w
+      tooltip.object.anchor_x = 1
+    else
+      tooltip.object.anchor_x = 0
+    end
+    tooltip.calculate_layout
   end
 
   def render
-    outputs.primitives << state.cells
-
+    outputs.primitives << background.primitives
+    outputs.primitives << @back_button.primitives
     outputs.primitives << party.primitives
+    outputs.primitives << tooltip.primitives
     # outputs.primitives << skills.primitives
     # outputs.primitives << minimap.primitives
 
     if debug?
-      [*party.primitives, *skills.primitives, *minimap.primitives].each do |rect|
-        outputs.debug << rect.to_s
+      outputs.primitives << [*party.interactive_nodes].map do |node|
+        {
+          **node.object,
+          r: 0,
+          g: 255,
+          b: 0
+        }.border!
       end
     end
   end
